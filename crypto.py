dee4288c409cf6cb1a2d5f11520c6be212f20758
davidbzhao <dz6hu@virginia.edu>
2019-04-12 18:44:08 -0400

    Add single line prints for each action and format doc

--- crypto.py
+++ crypto.py
@@ -53,7 +53,6 @@ def get_keys_from_file(infilepath):
     return public_key, private_key
 
 
-
 def add_transaction_to_ledger(transaction):
     """Write transaction record to ledger from transaction object."""
     transaction.pop('signature', None)
@@ -62,18 +61,27 @@ def add_transaction_to_ledger(transaction):
 
 
 def genesis(outfilepath):
-    """Generate genesis block in specified file."""
-    # TODO: disallow writing to bad place, for example, 'crypto.py'
+    """Generate genesis block in specified file.
+
+    Args:
+        outfilepath (str): Location to write genesis block
+    """
     with open(outfilepath, 'w') as outfile:
         outfile.write("Now watch closely, everyone.\n"
-            "I'm going to show you how to kill a god.\n"
-            "A god of life and death.\n"
-            "The trick is not to fear him.")
+                      "I'm going to show you how to kill a god.\n"
+                      "A god of life and death.\n"
+                      "The trick is not to fear him.")
 
 
 def generate(outfilepath):
-    """Generate a new wallet i.e. RSA public/private keyset."""
-    # TODO: disallow writing to bad place, for example, 'crypto.py'
+    """Generate a new wallet i.e. RSA public/private keyset.
+
+    Args:
+        outfilepath (str): Location to write wallet file  
+
+    Returns:
+        str: The wallet address.
+    """
     public_key, private_key = rsa.newkeys(1024)
     public_key_bytes = public_key.save_pkcs1(format='PEM')
     private_key_bytes = private_key.save_pkcs1(format='PEM')
@@ -82,11 +90,18 @@ def generate(outfilepath):
     with open(outfilepath, 'w') as outfile:
         outfile.write(public_key_string)
         outfile.write(private_key_string)
-
+    return address(outfilepath)
 
 
 def address(wallet_filepath):
-    """Return address of wallet."""
+    """Compute the shortened hash of wallet's public key.
+
+    Args:
+        wallet_filepath (str): Path to wallet  
+
+    Returns:
+        str: The wallet address.
+    """
     with open(wallet_filepath, 'r') as infile:
         data = infile.read()
         public_key = rsa.PublicKey.load_pkcs1(data)
@@ -98,12 +113,31 @@ def address(wallet_filepath):
 
 
 def fund(dest_wallet_addr, amount, outfilepath):
-    """Add some amount of value to a wallet."""
+    """Add some amount of value to a wallet.
+
+    Args:
+        dest_wallet_addr (str): Address of receiving wallet
+        amount (int): Address of wallet
+        outfilepath (str): Location to write transaction file  
+
+    Returns:
+        obj: The transaction object.
+    """
     return transfer(SPECIAL_ID, dest_wallet_addr, amount, outfilepath)
 
 
 def transfer(src_wallet_filepath, dest_wallet_addr, amount, outfilepath):
-    """Transfer some amount of value from one wallet to another."""
+    """Transfer some amount of value from one wallet to another.
+
+    Args:
+        src_wallet_filepath (str): Path to sending wallet
+        dest_wallet_addr (str): Address of receiving wallet
+        amount (int): Address of wallet
+        outfilepath (str): Location to write transaction file  
+
+    Returns:
+        obj: The transaction object.
+    """
     transaction = {
         'from': SPECIAL_ID,
         'to': dest_wallet_addr,
@@ -120,10 +154,18 @@ def transfer(src_wallet_filepath, dest_wallet_addr, amount, outfilepath):
 
     with open(outfilepath, 'w') as outfile:
         outfile.write(json.dumps(transaction, indent=4, sort_keys=True))
+    return transaction
 
 
 def balance(wallet_addr):
-    """Compute the available balance of a wallet."""
+    """Compute the available balance of a wallet.
+
+    Args:
+        wallet_addr (str): Address of wallet
+
+    Returns:
+        int: The current value of wallet.
+    """
     balance = 0
 
     filepaths_to_check = glob.glob('block_*[1-9]*.txt')  # except block_0.txt
@@ -147,7 +189,15 @@ def balance(wallet_addr):
 
 
 def verify(wallet_filepath, transaction_filepath):
-    """Verify transaction with given wallet credentials."""
+    """Verify transaction with given wallet credentials.
+
+    Args:
+        wallet_filepath (str): Path to wallet
+        transaction_filepath (str): Path to transaction  
+
+    Returns:
+        bool: If the transaction was successfully verified.
+    """
     def is_valid_transaction(transaction):
         if transaction['from'] == SPECIAL_ID:
             return True
@@ -164,15 +214,24 @@ def verify(wallet_filepath, transaction_filepath):
         except rsa.pkcs1.VerificationError as e:
             return False
         return True
-    
+
     with open(transaction_filepath, 'r') as infile:
         transaction = json.load(infile)
     if wallet_filepath == SPECIAL_ID or is_valid_transaction(transaction):
         add_transaction_to_ledger(transaction)
+        return True
+    return False
 
 
 def validate():
-    """Check the hashes of each block in the chain."""
+    """Check the hashes of each block in the chain.
+
+    Returns:
+        int: The number of the first block that has an inconsistent hash.
+
+        If there is no faulty blocks, then return -1. An inconsistent hash
+        is one that does not match the hash of the previous block.
+    """
     def compute_hash_of_block(block_number):
         block_filepath = 'block_{}.txt'.format(block_number)
         with open(block_filepath, 'r') as infile:
@@ -189,7 +248,6 @@ def validate():
         block_filepath = 'block_{}.txt'.format(block_number)
         return os.path.isfile(block_filepath)
 
-
     block_number = 0
     while True:
         if block_number > 0:
@@ -198,13 +256,21 @@ def validate():
             recorded_hash = get_recorded_hash_in_block(block_number)
             computed_hash = compute_hash_of_block(block_number - 1)
             if computed_hash != recorded_hash:
-                return False
+                return block_number
         block_number += 1
-    return True
+    return -1
 
 
 def mine(difficulty):
-    """Clear ledger by finding nonce and creating block."""
+    """Clear ledger by finding nonce and creating block.
+
+    Args:
+        difficulty (int): The number of leading zeros the nonce
+        must have.
+
+    Returns:
+        str: The hash of the newly-created block.
+    """
     def is_ledger_empty():
         with open(LEDGER_FILEPATH, 'r') as infile:
             return infile.read().strip() == ''
@@ -229,7 +295,7 @@ def mine(difficulty):
             return infile.read().strip()
 
     def add_nonce(next_block_contents, difficulty):
-        for nonce in range(2**64-1):
+        for nonce in range(2**64 - 1):
             new_block = next_block_contents + str(nonce)
             new_block_hash = hash_string(new_block)
             if new_block_hash[:difficulty] == '0' * difficulty:
@@ -243,32 +309,43 @@ def mine(difficulty):
     def clear_ledger():
         open(LEDGER_FILEPATH, 'w').close()
 
-    if not is_ledger_empty():
-        prev_block_number = get_previous_block_number()
-        prev_block_filepath = 'block_{}.txt'.format(prev_block_number)
-        prev_block_hash = get_block_hash(prev_block_filepath)
-        ledger_contents = get_ledger_contents()
-        
-        next_block_number = prev_block_number + 1
-        next_block_filepath = 'block_{}.txt'.format(next_block_number)
-        next_block_contents = prev_block_hash + '\n' + ledger_contents + '\n'
-        next_block_contents = add_nonce(next_block_contents, difficulty)
-        
-        write_block(next_block_filepath, next_block_contents)
-        clear_ledger()
+    if is_ledger_empty():
+        return None
+
+    prev_block_number = get_previous_block_number()
+    prev_block_filepath = 'block_{}.txt'.format(prev_block_number)
+    prev_block_hash = get_block_hash(prev_block_filepath)
+    ledger_contents = get_ledger_contents()
+
+    next_block_number = prev_block_number + 1
+    next_block_filepath = 'block_{}.txt'.format(next_block_number)
+    next_block_contents = prev_block_hash + '\n' + ledger_contents + '\n'
+    next_block_contents = add_nonce(next_block_contents, difficulty)
+    next_block_hash = hash_string(next_block_contents)
+
+    write_block(next_block_filepath, next_block_contents)
+    clear_ledger()
+    return next_block_hash
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('action')
-    parser.add_argument('--wallet', nargs='?', default=None, help='Wallet filepath')
-    parser.add_argument('--address', nargs='?', default=None, help='Wallet address')
-    parser.add_argument('--transaction', nargs='?', default=None, help='Transaction filepath')
-    parser.add_argument('--src', nargs='?', default=None, help='Source wallet filepath')
-    parser.add_argument('--dest', nargs='?', default=None, help='Destination wallet address')
+    parser.add_argument('--wallet', nargs='?',
+                        default=None, help='Wallet filepath')
+    parser.add_argument('--address', nargs='?',
+                        default=None, help='Wallet address')
+    parser.add_argument('--transaction', nargs='?',
+                        default=None, help='Transaction filepath')
+    parser.add_argument('--src', nargs='?', default=None,
+                        help='Source wallet filepath')
+    parser.add_argument('--dest', nargs='?', default=None,
+                        help='Destination wallet address')
     parser.add_argument('--out', nargs='?', default=None, help='Out file path')
-    parser.add_argument('--amount', nargs='?', default=None, type=int, help='Amount to be transferred')
-    parser.add_argument('--difficulty', nargs='?', default=None, type=int, help='Difficulty setting for mining')
+    parser.add_argument('--amount', nargs='?', default=None,
+                        type=int, help='Amount to be transferred')
+    parser.add_argument('--difficulty', nargs='?', default=None,
+                        type=int, help='Difficulty setting for mining')
     args = parser.parse_args()
 
     action = args.action
@@ -276,10 +353,12 @@ if __name__ == '__main__':
         if args.out == None:
             parser.error('--out must be specified')
         genesis(outfilepath=args.out)
+        print('[SUCCESS] In the beginning God created the heaven and the earth... and we created a cryptocurrency.')
     elif action == 'generate':
         if args.out == None:
             parser.error('--out must be specified')
-        generate(outfilepath=args.out)
+        wallet_addr = generate(outfilepath=args.out)
+        print('[SUCCESS] A wild WALLET ({}) has appeared!'.format(wallet_addr))
     elif action == 'address':
         if args.wallet == None:
             parser.error('--wallet must be specified')
@@ -292,7 +371,9 @@ if __name__ == '__main__':
             parser.error('--out must be specified')
         if args.amount == None:
             parser.error('--amount must be specified')
-        fund(args.dest, args.amount, args.out)
+        transaction = fund(args.dest, args.amount, args.out)
+        print(
+            '[SUCCESS] [{date}] {from} ---({amount} SAN)--> {to}'.format(**transaction))
     elif action == 'transfer':
         if args.src == None:
             parser.error('--src must be specified')
@@ -302,7 +383,9 @@ if __name__ == '__main__':
             parser.error('--out must be specified')
         if args.amount == None:
             parser.error('--amount must be specified')
-        transfer(args.src, args.dest, args.amount, args.out)
+        transaction = transfer(args.src, args.dest, args.amount, args.out)
+        print(
+            '[SUCCESS] [{date}] {from} ---({amount} SAN)--> {to}'.format(**transaction))
     elif action == 'balance':
         if args.address == None:
             parser.error('--address must be specified')
@@ -312,10 +395,31 @@ if __name__ == '__main__':
             parser.error('--wallet must be specified')
         if args.transaction == None:
             parser.error('--transaction must be specified')
-        verify(wallet_filepath=args.wallet, transaction_filepath=args.transaction)
+        verified = verify(wallet_filepath=args.wallet,
+                          transaction_filepath=args.transaction)
+        if verified:
+            print('[SUCCESS] LGTM! The transaction ({}) is now part of the ledger ({})'.format(
+                args.transaction, LEDGER_FILEPATH))
+        else:
+            print('[FAILURE] Uh oh! The transaction ({}) could not be verified with the wallet ({})'.format(
+                args.transaction, args.wallet))
     elif action == 'mine':
         if args.difficulty == None:
             parser.error('--difficulty must be specified')
-        mine(difficulty=args.difficulty)
+        block_hash = mine(difficulty=args.difficulty)
+        if block_hash:
+            print('[SUCCESS] The hash ({}...{}) does indeed start with {} zeros'.format(
+                block_hash[:8],
+                block_hash[-8:],
+                args.difficulty
+            ))
+        else:
+            print(
+                '[FAILURE] The ledger was empty, let\'s all save some electricity and not go mining.')
     elif action == 'validate':
-        print(validate())
+        broken_block_number = validate()
+        if broken_block_number == -1:
+            print('[SUCCESS] Blocks are chained.')
+        else:
+            print('[FAILURE] The hash in block #{} does not equal the hash of the previous block.'.format(
+                broken_block_number))
